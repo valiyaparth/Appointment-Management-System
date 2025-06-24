9cef8a8bd1bbb9ee377faabb92249cb438c35e8f
Parth Valiya <valiyaparth1407@gmail.com>
2025-06-24 15:00:01 +0530

    Email Service added

---
+++ Backend/AMS/AMS.Core.Shared/EmailService/SendGridEmailSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace AMS.Core.Shared.EmailService
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public SendGridEmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string body)
+        {
+            var client = new SendGridClient(_configuration["SendGrid:ApiKey"]);
+            var from = new EmailAddress(_configuration["SendGrid:SenderEmail"], "AMS");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
